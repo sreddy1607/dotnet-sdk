956e0a8fbad12b977cfe04810fb0f64800a7c910
Jonathon Marolf <jmarolf@microsoft.com>
2023-02-21 14:14:29 -0800

    update vsix project authoring

--- build.ps1
+++ build.ps1
@@ -32,31 +32,6 @@ $PackagesPath = "$RepoRoot\packages"
 $env:NUGET_PACKAGES = $PackagesPath
 $DotnetCLIVersion = Get-Content "$RepoRoot\DotnetCLIVersion.txt"
 
-# Use a repo-local install directory (but not the bin directory because that gets cleaned a lot)
-if (!$env:DOTNET_INSTALL_DIR)
-{
-    $env:DOTNET_INSTALL_DIR="$RepoRoot\.dotnet_cli\"
-}
-
-if (!(Test-Path $env:DOTNET_INSTALL_DIR))
-{
-    mkdir $env:DOTNET_INSTALL_DIR | Out-Null
-}
-
-if ($Verbosity -eq 'diagnostic') {
-    $dotnetInstallVerbosity = "-Verbose"
-}
-
-# Install a stage 0
-$DOTNET_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1"
-Invoke-WebRequest $DOTNET_INSTALL_SCRIPT_URL -OutFile "$env:DOTNET_INSTALL_DIR\dotnet-install.ps1"
-
-& "$env:DOTNET_INSTALL_DIR\dotnet-install.ps1" -Version $DotnetCLIVersion $dotnetInstallVerbosity
-if($LASTEXITCODE -ne 0) { throw "Failed to install stage0" }
-
-# Put the stage0 on the path
-$env:PATH = "$env:DOTNET_INSTALL_DIR;$env:PATH"
-
 # Disable first run since we want to control all package sources
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 
@@ -70,42 +45,6 @@ if ($RealSign) {
     $signType = 'real'
 }
 
-$buildTarget = 'Build'
-if ($SkipTests) {
-    $buildTarget = 'BuildWithoutTesting'
-}
-
-if ($FullMSBuild)
-{
-    $env:DOTNET_SDK_TEST_MSBUILD_PATH = join-path $env:VSInstallDir "MSBuild\15.0\bin\MSBuild.exe"
-}
-
-$commonBuildArgs = echo $RepoRoot\build\build.proj /m:1 /nologo /p:Configuration=$Configuration /p:Platform=$Platform /p:SignType=$signType /verbosity:$Verbosity
-
-# NET Core Build 
-$msbuildSummaryLog = Join-Path -path $logPath -childPath "sdk.log"
-$msbuildWarningLog = Join-Path -path $logPath -childPath "sdk.wrn"
-$msbuildFailureLog = Join-Path -path $logPath -childPath "sdk.err"
-
-dotnet msbuild /t:$buildTarget $commonBuildArgs /flp1:Summary`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildSummaryLog /flp2:WarningsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildWarningLog /flp3:ErrorsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildFailureLog
-if($LASTEXITCODE -ne 0) { throw "Failed to build" }
-
-# Template Build
-$msbuildSummaryLog = Join-Path -path $logPath -childPath "templates.log"
-$msbuildWarningLog = Join-Path -path $logPath -childPath "templates.wrn"
-$msbuildFailureLog = Join-Path -path $logPath -childPath "templates.err"
-
-msbuild /t:restore /p:RestorePackagesPath=$PackagesPath $RepoRoot\sdk-templates.sln /verbosity:$Verbosity
-if($LASTEXITCODE -ne 0) { throw "Failed to restore nuget packages for templates" }
-
-msbuild  /t:$buildTarget $commonBuildArgs /nr:false /p:BuildTemplates=true /flp1:Summary`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildSummaryLog /flp2:WarningsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildWarningLog /flp3:ErrorsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildFailureLog
-if($LASTEXITCODE -ne 0) { throw "Failed to build templates" }
-
-# Modern VSIX build
-$msbuildSummaryLog = Join-Path -path $logPath -childPath "modernvsix.log"
-$msbuildWarningLog = Join-Path -path $logPath -childPath "modernvsix.wrn"
-$msbuildFailureLog = Join-Path -path $logPath -childPath "modernvsix.err"
-
-msbuild /t:BuildModernVsixPackages $commonBuildArgs /nr:false /p:BuildTemplates=true /flp1:Summary`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildSummaryLog /flp2:WarningsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildWarningLog /flp3:ErrorsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildFailureLog
-if($LASTEXITCODE -ne 0) { throw "Failed to build modern vsix packages" }
+msbuild /t:restore /p:RestorePackagesPath=$PackagesPath /m:1 /nologo /p:Configuration=$Configuration /p:Platform=$Platform /p:SignType=$signType $RepoRoot\src\VsixV3\PreEmptive.Solutions.Dotfuscator.CE.vsmanproj
+msbuild /p:RestorePackagesPath=$PackagesPath /m:1 /nologo /p:Configuration=$Configuration /p:Platform=$Platform /p:SignType=$signType $RepoRoot\src\VsixV3\PreEmptive.Solutions.Dotfuscator.CE.vsmanproj
 
