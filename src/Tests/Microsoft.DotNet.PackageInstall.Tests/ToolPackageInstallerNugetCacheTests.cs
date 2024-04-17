5d0a321ee51cb462d16fcdf16ffd995e24eacd15
annie <59816815+JL03-Yue@users.noreply.github.com>
2024-04-17 12:07:31 -0700

    fix tests

--- src/Tests/Microsoft.DotNet.PackageInstall.Tests/ToolPackageInstallerNugetCacheTests.cs
+++ src/Tests/Microsoft.DotNet.PackageInstall.Tests/ToolPackageInstallerNugetCacheTests.cs
@@ -43,7 +43,7 @@ namespace Microsoft.DotNet.PackageInstall.Tests
                     versionRange: VersionRange.Parse(TestPackageVersion),
                     packageLocation: new PackageLocation(nugetConfig: nugetConfigPath),
                     targetFramework: _testTargetframework,
-                    verifySigning: false);
+                    verifySignatures: false);
 
                 var commands = toolPackage.Commands;
                 var expectedPackagesFolder = NuGetGlobalPackagesFolder.GetLocation();
@@ -82,7 +82,7 @@ namespace Microsoft.DotNet.PackageInstall.Tests
                 versionRange: VersionRange.Parse("1.0.0-*"),
                 packageLocation: new PackageLocation(nugetConfig: nugetConfigPath),
                 targetFramework: _testTargetframework,
-                verifySigning: false);
+                verifySignatures: false);
 
             var expectedPackagesFolder = NuGetGlobalPackagesFolder.GetLocation();
 
